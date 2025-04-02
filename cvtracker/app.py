03f1ccad7e3f374e7fad20940b3d02312ff45f41
bentoluizv <bentoluizv@gmail.com>
2025-04-02 10:22:37 -0300

    Add static file serving and Jinja2 templating to FastAPI app

--- cvtracker/app.py
+++ cvtracker/app.py
@@ -1,8 +1,34 @@
-from fastapi import FastAPI
+import os
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+app.mount(
+    '/static',
+    StaticFiles(directory=os.path.join(os.getcwd(), 'static')),
+    name='static',
+)
 
-@app.get('/')
-def root():
-    return {'Hello': 'World'}
+templates = Jinja2Templates(directory='templates')
+
+
+@app.get('/', response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse(request=request, name='index.html')
+
+
+# @app.get('/todos', response_class=HTMLResponse)
+# async def list_todos(
+#     request: Request, hx_request: Annotated[Union[str, None], Header()] = None  # noqa: E501
+# ):
+#     if hx_request:
+#         return templates.TemplateResponse(
+#             request=request, name='todos.html', context={'todos': todos}
+#         )
+
+#     jsonable_todos = jsonable_encoder(todos)
+#     return JSONResponse(jsonable_todos)
